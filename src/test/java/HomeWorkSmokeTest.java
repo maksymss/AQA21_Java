2db7db431fec3d5299a9055d52d9959f7dd1497d
maksymss <maksym.sudarikovnyc@gmail.com>
2023-07-04 18:00:08 -0400

    selenium_wd_hw

---
+++ src/test/java/HomeWorkSmokeTest.java
@@ -0,0 +1,61 @@
+import factory.BrowserFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class HomeWorkSmokeTest {
+protected WebDriver driver;
+
+@BeforeMethod
+public void setUp() {
+    BrowserFactory browserFactory = new BrowserFactory();
+    driver = browserFactory.getDriver();
+}
+    @Test
+    public void electricFloorCalc() throws InterruptedException {
+        driver.get("https://kermi-fko.ru/raschety/Calc-Rehau-Solelec.aspx");
+
+        WebElement widthInput = driver.findElement(By.id("el_f_width"));
+        WebElement lengthInput = driver.findElement(By.id("el_f_lenght"));
+        WebElement heatingLossesInput = driver.findElement(By.id("el_f_losses"));
+        WebElement calcButton = driver.findElement(By.name("button"));
+
+
+
+        WebElement selectWebElementOne = driver.findElement(By.id("room_type"));
+        Select selectRoomType = new Select(selectWebElementOne);
+
+        selectRoomType.selectByValue("2");
+        Thread.sleep(Long.parseLong("2000"));
+
+        WebElement selectWebElementTwo = driver.findElement(By.id("heating_type"));
+        Select selectHeatingType = new Select(selectWebElementTwo);
+
+        selectHeatingType.selectByValue("3");
+        Thread.sleep(Long.parseLong("2000"));
+
+        widthInput.sendKeys("20");
+        lengthInput.sendKeys("20");
+        heatingLossesInput.sendKeys("38");
+        calcButton.click();
+
+        WebElement floorCablePowResult = driver.findElement(By.id("floor_cable_power"));
+        Assert.assertEquals(floorCablePowResult.getAttribute(), "21" );
+
+        //Cannot find Element to check the result
+
+        WebElement specCablePowResult = driver.findElement(By.id("spec_floor_cable_power"));
+        Assert.assertEquals(specCablePowResult.getAttribute("), "0");
+
+    }
+
+    @AfterMethod
+    public void tearDown() {
+    driver.quit();
+    }
+}
--- src/test/java/SmokeTest.java
+++ src/test/java/SmokeTest.java
@@ -51,15 +51,16 @@ public class SmokeTest {
         driver.get("https://13gp.by/%D0%B8%D0%BD%D1%84%D0%BE%D1%80%D0%BC%D0%B0%D1%86%D0%B8%D1%8F/%D0%BC%D0%B5%D0%B4%D0%B8%D1%86%D0%B8%D0%BD%D1%81%D0%BA%D0%B8%D0%B5-%D0%BA%D0%B0%D0%BB%D1%8C%D0%BA%D1%83%D0%BB%D1%8F%D1%82%D0%BE%D1%80%D1%8B/%D1%80%D0%B0%D1%81%D1%87%D0%B5%D1%82-%D1%81%D0%BA%D0%BE%D1%80%D0%BE%D1%81%D1%82%D0%B8-%D0%BA%D0%BB%D1%83%D0%B1%D0%BE%D1%87%D0%BA%D0%BE%D0%B2%D0%BE%D0%B9-%D1%84%D0%B8%D0%BB%D1%8C%D1%82%D1%80%D0%B0%D1%86%D0%B8%D0%B8-%D1%81%D0%BA%D1%84");
 
         WebElement selectWebElement = driver.findElement(By.id("oSex"));
-        Select selectSex = new Select(selectWebElement);
+        //Select selectSex = new Select(selectWebElement);
 /*
         selectSex.selectByIndex(1);
         Thread.sleep(2000);
+
         selectSex.selectByValue("0");
         Thread.sleep(2000);
 */
-        selectSex.selectByVisibleText("женский");
-        sleep(2000);
+       // selectSex.selectByVisibleText("женский");
+       // sleep(2000);
 
         driver.findElement(By.id("oCr")).sendKeys("80");
         driver.findElement(By.id("oAge")).sendKeys("38");
