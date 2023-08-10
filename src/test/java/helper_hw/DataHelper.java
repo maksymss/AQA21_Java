615584ee48a53b48f83aab0b3e6bc20d28f89019
maksymss <maksym.sudarikovnyc@gmail.com>
2023-08-09 21:40:50 -0400

    logger

--- src/test/java/helper_hw/DataHelper.java
+++ src/test/java/helper_hw/DataHelper.java
@@ -1,13 +1,20 @@
 package helper_hw;
 
 import models.User;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import tests.LoggerTest;
 import utils.configuration.ReadProperties;
 
 public class DataHelper {
+    static Logger logger = LogManager.getLogger(DataHelper.class);
+
     public static User getUser() {
         User user = new User();
         user.setEmail(ReadProperties.username());
         user.setPassword(ReadProperties.password());
+
+        logger.info(user.toString());
         return user;
     }
 }
